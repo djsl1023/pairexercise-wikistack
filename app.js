7e7476acb9ca484336c4ea3bee2461dc854f53ff
Dan Lee <dan.jisang.lee@gmail.com>
2022-01-21 15:05:57 -0500

    on schemas

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 const morgan = require('morgan');
 const express = require('express');
+const layout = require('./views/layout');
+const {db} = require('./models');
+
+const app = express();
+const port = 3000;
+
+//static
+app.use(express.static('public'));
+//morgan
+app.use(morgan("dev"));
+//body parser
+app.use(express.urlencoded({ extended: false }));
+
+db.authenticate().then(() =>{
+  console.log('connected to the database');
+})
+
+app.get('/', (req, res, next) => {
+  res.send(layout());
+})
+
+app.listen(port, function(){
+  console.log("Connecting")
+})
